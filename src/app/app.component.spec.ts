368a93c58440df26fd517b5dab9def3b71af8998
LuisCorrales <luis.corrales@tecsup.edu.pe>
2023-10-31 18:20:57 -0500

    cambios

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Lab08-ejemplo1'`, () => {
+  it(`should have as title 'Lab08app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Lab08-ejemplo1');
+    expect(app.title).toEqual('Lab08app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Lab08-ejemplo1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Lab08app app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Lab08-ejemplo1';
+  title = 'Lab08app';
+  nombre = 'Corrales Luis';
+  edad = 20;
+  email = 'luis.corrales@tecsup.edu.pe';
+  sueldos = [1700, 1600, 1900];
+  activo = true;
+  sitio = 'http://www.google.com.pe';
+  contador = 1;
+  profesion = '';
+  anios = '';
+  esActivo() {
+    if (this.activo) 
+       return 'Trabajador Activo';
+     else 
+       return 'Trabajador Inactivo';
+  }
+
+  ultimos3Sueldos() {
+    let suma = 0;
+    for (let x = 0; x < this.sueldos.length; x++)
+      suma += this.sueldos[x];
+    return suma;
+  }
+  incrementar() {
+    this.contador++;
+  }
+  decrementar() {
+    this.contador--;
+  }
 }
